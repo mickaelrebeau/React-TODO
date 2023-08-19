5534c6f01c097e74d6ae35ce18b9d187b3279c5e
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-08-19 20:23:07 +0200

    react-router-dom

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,14 +1,19 @@
 import { ThemeProvider } from "./components/theme-provider"
 import './App.css'
 import Home from "./pages/Home/HomePage"
-import { Navbar } from "./components/navbar"
+import Navbar  from "./components/navbar"
+import { Route, Routes } from "react-router-dom"
+import { CreateTodo } from "./pages/Todo/CreateTodoPage"
 
 function App() {
   return (
     <>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <Navbar />
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new-todo" element={<CreateTodo />} />
+        </Routes>
       </ThemeProvider>
     </>
   )
