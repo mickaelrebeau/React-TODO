35f73e01f2f82fa04f089ed0273027a3a336ab6c
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-08-20 21:58:27 +0200

    Api Call delete and update

--- Front/src/services/api/todo.ts
+++ Front/src/services/api/todo.ts
@@ -13,10 +13,37 @@ export async function getAllTodos() {
     }
 }
 
+export async function getTodo(id: any) {
+    try {
+        const todo = await api.get(`/todos/${id}`);
+        return todo;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 export async function createTodo(body: any) {
     try {
         const newTodo = await api.post("/todos", body);
         return newTodo;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function updateTodo(id: any, body: any) {
+    try {
+        const updatedTodo = await api.put(`/todos/${id}`, body);
+        return updatedTodo;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function deleteTodo(id: any) {
+    try {
+        const deletedTodo = await api.delete(`/todos/${id}`);
+        return deletedTodo;
     } catch (error) {
         console.log(error);
     }
