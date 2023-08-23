df8879841c81dc69110792ac66037e2569ff0022
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-08-23 11:32:43 +0200

    Home Page changes

--- Front/src/pages/Home/HomePage.tsx
+++ Front/src/pages/Home/HomePage.tsx
@@ -22,9 +22,9 @@ export default function Home() {
             <div className="hidden h-full flex-1 flex-col space-y-8 mt-8 p-8 md:flex">
                 <div className="flex items-center justify-between space-y-2">
                     <div className="text-left">
-                        <h2 className="text-2xl font-bold tracking-tight">Welcome!</h2>
+                        <h2 className="text-2xl font-bold tracking-tight text-accent">Welcome!</h2>
                         <p className="text-muted-foreground">
-                        Here&apos;s a list of your tasks for this month!
+                        Here&apos;s a list of your tasks!
                         </p>
                     </div>
                 </div>
