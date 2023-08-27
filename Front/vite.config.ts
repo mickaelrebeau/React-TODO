7cc8ed21383358c5cea45cc739a95969102c0ead
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-08-27 21:22:04 +0200

    Vitest setup

--- Front/vite.config.ts
+++ Front/vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
@@ -5,6 +7,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: [path.resolve(__dirname, "test/setup.ts")],
+    env: {
+        IS_REACT_ACT_ENVIRONMENT: 'true',
+    },
+    include: ['__tests__/**/*.[jt]s?(x)'],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "."),
